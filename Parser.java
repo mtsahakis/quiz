32c054aca3e5d498800f2cead00fc72f2c06e8a8
Manos Tsahakis <mtsahakis@gmail.com>
2016-12-14 22:31:11 +0000

    Update Parser.java

--- Parser.java
+++ Parser.java
@@ -7,36 +7,35 @@ import java.io.IOException;
  */
 public class Parser {
   private File file;
-  public synchronized void setFile(File f) {
+  public void setFile(File f) {
     file = f;
   }
-  public synchronized File getFile() {
+  public File getFile() {
     return file;
   }
   public String getContent() throws IOException {
-    FileInputStream i = new FileInputStream(file);
-    String output = "";
-    int data;
-    while ((data = i.read()) > 0) {
-      output += (char) data;
-    }
-    return output;
+    return getData(true);
   }
   public String getContentWithoutUnicode() throws IOException {
-    FileInputStream i = new FileInputStream(file);
+    return getData(false);
+  }
+  public void saveContent(String content) throws IOException {
+    FileOutputStream fos = new FileOutputStream(file);
+    for (int i = 0; i < content.length(); i += 1) {
+      fos.write(content.charAt(i));
+    }
+    fos.close();
+  }
+  private String getData(boolean parseUnicode) throws IOException {
+    FileInputStream fis = new FileInputStream(file);
     String output = "";
     int data;
-    while ((data = i.read()) > 0) {
-      if (data < 0x80) {
+    while ((data = fis.read()) > 0) {
+      if (parseUnicode || data < 0x80) {
         output += (char) data;
       }
     }
+    fis.close();
     return output;
-  }
-  public void saveContent(String content) throws IOException {
-    FileOutputStream o = new FileOutputStream(file);
-    for (int i = 0; i < content.length(); i += 1) {
-      o.write(content.charAt(i));
-    }
   }
 }
